d8eb7cfeb20ffdbd2a7d8756fda9b1e7d7fc9058
Artur Shkekin <64356091+ArturShkekin@users.noreply.github.com>
2021-05-27 20:51:22 +0300

    аа

--- js/index.js
+++ js/index.js
@@ -8,11 +8,12 @@ class CountdownTimer {
         this.hoursSpan = document.querySelector(`${selector}.value[data-value="hours"]`);
         this.minutesSpan = document.querySelector(`${selector}.value[data-value="mins"]`);
         this.secondsSpan = document.querySelector(`${selector}.value[data-value="secs"]`);
+
     }
+
     pad(value) {
         return String(value).padStart(2, "0");
     }
-
     countDowm() {
         const currentTime = new Date();
         this.createSpanValue(currentTime);
@@ -24,21 +25,24 @@ class CountdownTimer {
 
     createSpanValue(currentTime) {
         const time = this.targetDate - currentTime;
-        this.daysSpan = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-        this.hoursSpan = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-        this.minutesSpan = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-        this.secondsSpan = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+        this.daysSpan.textContent = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        this.hoursSpan.textContent = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        this.minutesSpan.textContent = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        this.secondsSpan.textContent = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
     }
+    
 }
 
 const timer = new CountdownTimer({
-  selector: '#timer-1',
+  selector: "#timer-1",
   targetDate: "2022,1,1",
 });
 
 document.body.onload = startTimer;
 
 function startTimer() {
+
     timer.showTime();
 }
 
